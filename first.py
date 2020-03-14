622892eb9119454454e7339bde8315805dffebfd
John MacTavish <john@techsolx.com>
2020-03-14 00:40:22 +0000

    Add f-string print example.

--- first.py
+++ first.py
@@ -2,12 +2,15 @@
 
 # Any line that begins with a # is a comment.
 
-# Use a Python built in function to do something:
-# https://docs.python.org/3/library/functions.html
-# We will use input() and print()
-# and a variable named, name.
+''' This is a block comment
+Use a Python built in function to do something:
+https://docs.python.org/3/library/functions.html
+We will use input() and print()
+and a variable named, name.
+'''
 
 name = input('Please type in a name: ')
 print('Hello, ' + name)
-print('Hello, {}'.format(name))
 print('Hello, %s' % name)
+print('Hello, {}'.format(name))
+print(f'Hello, {name}')
